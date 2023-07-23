a2b5b77cc7bb17e600eabd97304b5a72ce7b1cd4
MartinsF3lipe <fo72983@gmail.com>
2023-07-23 11:54:18 -0300

    push_3.0

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,31 +1,42 @@
+//elements utility
 const inputTask = document.getElementById("inputTask")
-const buttonAddTask = document.getElementById("addtask")
-const buttonCompleteTask = document.getElementById("completeTask")
-const taskManager = document.getElementById("taskmanager")
+const btnAddTask = document.getElementById("btnAddTask")
+const localStorageKey = 'ToDoList'
 
-buttonAddTask.addEventListener("click", () => {
+//function utility
+function newTaskAdd() {
     if (inputTask.value) {
-        const newTask = document.createElement("div")
-        newTask.classList.add("tasks")
-
-        const textTask = document.createElement("h3")
-        textTask.innerText = inputTask.value
-        newTask.appendChild(textTask)
-
-        const buttonComplete = document.createElement("button")
-        buttonComplete.innerHTML =  '<ion-icon name="checkmark-circle-sharp"></ion-icon>'
-        buttonComplete.id = "completeTask"
-        newTask.appendChild(buttonComplete)
-
-        taskManager.appendChild(newTask)
+        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+        values.push({
+            name: inputTask.value
+        })
+        localStorage.setItem(localStorageKey, JSON.stringify(values))
+        showTasks()
     }
-});
-
-buttonCompleteTask.addEventListener("click", (e) => {
-    const tasks = taskManager.getElementsByTagName("div");
-    for (const task of tasks) {
-        task.addEventListener("click", () => {
-            task.classList.toggle('completeTasks');
-        });
+    else {
+        window.alert("Adicione um valor no campo!")
     }
-});
+};
+
+function showTasks () {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]")
+    let listTask = document.getElementById("ToDoList")
+    listTask.innerHTML = ""
+    for (let i = 0; i < values.length; i++) {
+        listTask.innerHTML += `<li>${values[i]['name']}<button onclick="taskCompleted('${values[i]['name']}')" id="completeTask"><ion-icon name="checkmark-circle-sharp"></ion-icon></button></li>`
+    }
+}
+
+function taskCompleted(data) {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || '[]')
+    let index = values.findIndex(x => x.name == data)
+    values.splice(index,1)
+    localStorage.setItem(localStorageKey, JSON.stringify(values))
+    showTasks()
+}
+
+//events utility
+btnAddTask.addEventListener("click", newTaskAdd);
+
+//init utility 
+showTasks()
